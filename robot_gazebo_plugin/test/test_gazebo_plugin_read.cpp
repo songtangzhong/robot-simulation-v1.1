7aa3ae9d334c52f894ff8d779baf10977cfe068e
Song Tangzhong <songtangzhong@163.com>
2021-05-01 20:13:30 +0800

    robot_gazebo_plugin

--- robot_gazebo_plugin/test/test_gazebo_plugin_read.cpp
+++ robot_gazebo_plugin/test/test_gazebo_plugin_read.cpp
@@ -1,9 +1,6 @@
-/*
-    This file is used to test gazebo control plugin according to reading operation.
-*/
-
 #include <robot_info/robot_info.h>
 #include <shared_memory/shared_memory.h>
+#include <shared_memory/semaphore.h>
 #include <rclcpp/rclcpp.hpp>
 #include <iostream>
 
@@ -14,49 +11,92 @@ int main(int argc, char ** argv)
     // Store the information of robot.
     std::shared_ptr<robot_info::RobotInfo> robot = std::make_shared<robot_info::RobotInfo>();
 
-    // pointer to shared memory.
-    shm::Shm *shm_ptr;
-    // ID to shared memory.
-    int shm_id;
+    // pointer to arm shared memory.
+    shm::ArmShm *arm_shm_ptr;
+    // ID to arm shared memory.
+    int arm_shm_id;
 
-    shm_id = shm::create_shm(&shm_ptr);
-    if (shm_id != PROCESS_STATE_NO)
+    arm_shm_id = shm::create_shm(robot->arm_shm_key_, &arm_shm_ptr);
+    if (arm_shm_id != SHM_STATE_NO)
     {
-        std::cout << "Create shared memory successfully." << std::endl;
-        std::cout << "shm_id: " << shm_id << std::endl;
+        std::cout << "Create arm shared memory successfully." << std::endl;
+        std::cout << "arm_shm_id: " << arm_shm_id << std::endl;
     }
     else
     {
-        std::cout << "Create shared memory failed." << std::endl;
+        std::cout << "Create arm shared memory failed." << std::endl;
         return 0;
     }
 
-    // ID to semaphore.
-    int sem_id;
+    // ID to arm semaphore.
+    int arm_sem_id;
 
-    sem_id = sem::create_semaphore();
-    if (sem_id != PROCESS_STATE_NO)
+    arm_sem_id = sem::create_semaphore(robot->arm_sem_key_);
+    if (arm_sem_id != SEM_STATE_NO)
     {
-        std::cout << "Create semaphore successfully." << std::endl;
-        std::cout << "sem_id: " << sem_id << std::endl;
+        std::cout << "Create arm semaphore successfully." << std::endl;
+        std::cout << "arm_sem_id: " << arm_sem_id << std::endl;
     }
     else
     {
-        std::cout << "Create semaphore failed." << std::endl;
+        std::cout << "Create arm semaphore failed." << std::endl;
         return 0;
     }
+
+#if END_EFFECTOR_TRUE
+    // pointer to end-effector shared memory.
+    shm::EndEffShm *end_eff_shm_ptr;
+    // ID to end-effector shared memory.
+    int end_eff_shm_id;
+
+    end_eff_shm_id = shm::create_shm(robot->end_eff_shm_key_, &end_eff_shm_ptr);
+    if (end_eff_shm_id != SHM_STATE_NO)
+    {
+        std::cout << "Create end-effector shared memory successfully." << std::endl;
+        std::cout << "end_eff_shm_id: " << end_eff_shm_id << std::endl;
+    }
+    else
+    {
+        std::cout << "Create end-effector shared memory failed." << std::endl;
+        return 0;
+    }
+
+    // ID to end-effector semaphore.
+    int end_eff_sem_id;
+
+    end_eff_sem_id = sem::create_semaphore(robot->end_eff_sem_key_);
+    if (end_eff_sem_id != SEM_STATE_NO)
+    {
+        std::cout << "Create end-effector semaphore successfully." << std::endl;
+        std::cout << "end_eff_sem_id: " << end_eff_sem_id << std::endl;
+    }
+    else
+    {
+        std::cout << "Create end-effector semaphore failed." << std::endl;
+        return 0;
+    }
+#endif
 
     rclcpp::WallRate loop_rate(1000);
     while (rclcpp::ok())
     {
-        sem::semaphore_p(sem_id);
+        sem::semaphore_p(arm_sem_id);
         for (unsigned int j=0; j< robot->arm_dof_; j++)
         { 
-            std::cout << "cur_joint_positions_[" << j << "]: " << shm_ptr->cur_joint_positions_[j] << std::endl;
-            std::cout << "cur_joint_velocities_[" << j << "]: " << shm_ptr->cur_joint_velocities_[j] << std::endl;
-            std::cout << "cur_joint_efforts_[" << j << "]: " << shm_ptr->cur_joint_efforts_[j] << std::endl;
+            std::cout << "cur_arm_joint_positions_[" << j << "]: " << arm_shm_ptr->cur_arm_joint_positions_[j] << std::endl;
+            std::cout << "cur_arm_joint_velocities_[" << j << "]: " << arm_shm_ptr->cur_arm_joint_velocities_[j] << std::endl;
+            std::cout << "cur_arm_joint_efforts_[" << j << "]: " << arm_shm_ptr->cur_arm_joint_efforts_[j] << std::endl;
         }
-        sem::semaphore_v(sem_id);
+        sem::semaphore_v(arm_sem_id);
+
+        sem::semaphore_p(end_eff_sem_id);
+        for (unsigned int j=0; j< robot->end_eff_dof_; j++)
+        { 
+            std::cout << "cur_end_eff_joint_positions_[" << j << "]: " << end_eff_shm_ptr->cur_end_eff_joint_positions_[j] << std::endl;
+            std::cout << "cur_end_eff_joint_velocities_[" << j << "]: " << end_eff_shm_ptr->cur_end_eff_joint_velocities_[j] << std::endl;
+            std::cout << "cur_end_eff_joint_efforts_[" << j << "]: " << end_eff_shm_ptr->cur_end_eff_joint_efforts_[j] << std::endl;
+        }
+        sem::semaphore_v(end_eff_sem_id);
         loop_rate.sleep();
     }
 
